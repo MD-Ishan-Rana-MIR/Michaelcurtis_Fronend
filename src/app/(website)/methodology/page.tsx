a27531ff5d318741fdc949a936719e2294baadaa
ishanrana363 <ishanrana363@gmail.com>
2025-09-11 17:37:25 +0600

    contact page compelted

---
+++ src/app/(website)/methodology/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Methodology from './Methodology'
+
+const Page = () => {
+    return (
+        <div className=' bg-[#f9fafb] ' >
+            <Methodology></Methodology>
+        </div>
+    )
+}
+
+export default Page
