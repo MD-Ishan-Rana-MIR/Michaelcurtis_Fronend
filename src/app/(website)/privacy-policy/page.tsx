0c5de729d515ecdfb1fd032acd81da7dd3b90491
ishanrana363 <ishanrana363@gmail.com>
2025-09-13 09:35:13 +0600

    Privacy page completed

---
+++ src/app/(website)/privacy-policy/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PrivacyPolicy from './PrivacyPolicy'
+
+const Page: React.FC = () => {
+    return (
+        <div className=' bg-[#f9fafb] ' >
+            <PrivacyPolicy></PrivacyPolicy>
+        </div>
+    )
+}
+
+export default Page
---
+++ src/app/(website)/term-service/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TermBanner from './TermBanner'
+
+const Page: React.FC = () => {
+    return (
+        <div className=' bg-[#f9fafb] ' >
+            <TermBanner></TermBanner>
+        </div>
+    )
+}
+
+export default Page
