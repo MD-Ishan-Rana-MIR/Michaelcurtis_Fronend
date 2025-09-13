6b742bf4f4d5e6abc2b5fa7383566bb210db1017
ishanrana363 <ishanrana363@gmail.com>
2025-09-13 09:53:41 +0600

    term page completed

--- src/app/(website)/term-service/page.tsx
+++ src/app/(website)/term-service/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import TermBanner from './TermBanner'
+import TermText from './TermText'
 
 const Page: React.FC = () => {
     return (
         <div className=' bg-[#f9fafb] ' >
             <TermBanner></TermBanner>
+            <TermText></TermText>
         </div>
     )
 }
