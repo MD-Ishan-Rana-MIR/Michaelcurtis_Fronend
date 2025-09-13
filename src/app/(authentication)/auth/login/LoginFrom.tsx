3752b5dc7fb9fc739d2702f7702016323245183b
ishanrana363 <ishanrana363@gmail.com>
2025-09-13 13:13:54 +0600

    join login page completed

--- src/app/(authentication)/auth/login/LoginFrom.tsx
+++ src/app/(authentication)/auth/login/LoginFrom.tsx
@@ -7,76 +7,90 @@ import { Eye, EyeOff } from "lucide-react";
 import Link from "next/link";
 
 export default function LoginForm() {
-    const [showPassword, setShowPassword] = useState(false);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [rememberMe, setRememberMe] = useState(false);
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+    const [email, setEmail] = useState<string | undefined>("");
+    const [password, setPassword] = useState<string | undefined>("");
+    const [rememberMe, setRememberMe] = useState<boolean>(false);
+
 
     return (
-        <div className=" bg-[#faf5ec]  py-11 min-h-screen " >
+        <div className="     max-w-4xl mx-auto  " >
             <MaxWidth>
-                <div className=" flex flex-col md:flex-row space-y-5 lg:gap-x-24 md:gap-x-10 ">
+                <div>
+                    <Link href={"/"}>
+                        <div className=" flex justify-center " >
+                            <Image src={"/images/logo/logo-svg.svg"} width={316} height={69} alt="" className=" flex justify-center  " />
+                        </div>
+                    </Link>
+                </div>
+                <div className="bg-[#faf5ec] lg:px-14 px-7 lg:pt-12 pt-6 lg:pb-9 pb-4 shadow shadow-[#00000033] border border-[#FAF5EC] rounded-2xl  ">
 
                     {/* Left Side: Form */}
-                    <div className="flex-1 w-full  lg:max-w-[45%]    ">
-                        <div className="">
-                            <div>
-                                {/* logo  */}
-                                <div>
-                                    <Image src={"/images/logo/logo.png"} width={1000} height={1000} alt="" className=" flex justify-center  " />
-                                </div>
-                            </div>
-                            <div className=" mt-7 lg:mt-14 text-center  " >
-                                <h1 className=" font-bold lg:text-4xl text-[#1F2937] " >Welcome back!</h1>
-                                <p className=" lg:mt-4 mt-2 font-normal text-[#697079] text-[16px] " >
-                                    Please enter the below information to logged in.
-                                </p>
 
-                            </div>
-                            <div className=" lg:mt-16 mt-8 " >
-                            </div>
+                    <div className="">
+                        <div>
+                            {/* logo  */}
+
+                        </div>
+                        <div className=" mt-7 lg:mt-14 text-center  " >
+                            <h1 className=" font-normal lg:text-5xl text-xl text-[#000000] " >Login to your Account</h1>
+                            <p className=" lg:mt-5 mt-2.5 font-normal text-[#000000] lg:text-xl text-xs  " >
+                                Welcome back Access your profile and manage your reviews
+                            </p>
+
+                        </div>
+
+                        <div className=" lg:mt-10 mt-5 " >
                             <form className="space-y-4">
-                                {/* Email */}
+
+
+
                                 <div>
-                                    <label
-                                        className="text-[#1F2937] font-bold lg:text-lg mb-2"
-                                        htmlFor="email"
-                                    >
-                                        Email
-                                    </label>
-                                    <input
-                                        type="email"
-                                        id="email"
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        className="w-full px-4 py-3 border-4 border-[#F8F2E5] focus:outline-none focus:ring-0 bg-white rounded-[47px]  "
-                                        placeholder="Enter your email"
-                                        style={{
-                                            boxShadow: "0 4px 10px rgba(248, 242, 229, 0.8)", // custom shadow color
-                                        }}
-                                    />
+                                    {/* Email */}
+                                    <div>
+                                        <label
+                                            className="text-[#000000] mb-4 block lg:text-xl text-xs  font-normal"
+                                            htmlFor="email"
+                                        >
+                                            Email
+                                        </label>
+                                        <input
+                                            type="email"
+                                            id="email"
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                            className="w-full px-4 py-4 border-1 border-[#989DA3] focus:outline-none focus:ring-0   rounded-[10px] "
+                                            // placeholder="Enter your email"
+                                            style={{
+                                                boxShadow: "0 4px 10px rgba(248, 242, 229, 0.8)", // custom shadow color
+                                            }}
+                                        />
+                                    </div>
                                 </div>
 
-                                {/* Password */}
-                                <div>
-                                    <label className="text-[#1F2937] font-bold lg:text-lg mb-2" htmlFor="password">Password</label>
-                                    <div className="relative">
-                                        <input
-                                            type={showPassword ? "text" : "password"}
-                                            id="password"
-                                            value={password}
-                                            onChange={(e) => setPassword(e.target.value)}
-                                            className="w-full px-4 py-3 border-4 border-[#F8F2E5] focus:outline-none focus:ring-0 bg-white rounded-[47px]  "
-                                            placeholder="********"
-                                        />
-                                        <button
-                                            type="button"
-                                            onClick={() => setShowPassword(!showPassword)}
-                                            className="absolute right-3 top-1/2 transform -translate-y-1/2 px-6 "
-                                        >
-                                            {showPassword ? <EyeOff className=" cursor-pointer " /> : <><Eye className=" cursor-pointer " /></>}
-                                        </button>
+                                <div className="   " >
+                                    {/* Password */}
+                                    <div className=" flex-1 " >
+                                        <label className="text-[#000000] mb-4 block lg:text-xl text-xs  font-normal" htmlFor="password">Password</label>
+                                        <div className="relative">
+                                            <input
+                                                type={showPassword ? "text" : "password"}
+                                                id="password"
+                                                value={password}
+                                                onChange={(e) => setPassword(e.target.value)}
+                                                className="w-full px-4 py-4 border-1 border-[#989DA3] focus:outline-none focus:ring-0   rounded-[10px]   "
+                                            // placeholder="********"
+                                            />
+                                            <button
+                                                type="button"
+                                                onClick={() => setShowPassword(!showPassword)}
+                                                className="absolute right-3 top-1/2 transform -translate-y-1/2 px-6 "
+                                            >
+                                                {showPassword ? <EyeOff className=" cursor-pointer " /> : <><Eye className=" cursor-pointer " /></>}
+                                            </button>
+                                        </div>
                                     </div>
+
                                 </div>
 
                                 {/* Remember Me + Forgot Password */}
@@ -90,36 +104,30 @@ export default function LoginForm() {
                                         />
                                         <span className="ml-2 text-sm">Remember me</span>
                                     </label>
-                                    <a href="#" className="text-sm text-blue-600 hover:underline">Forgot password?</a>
+                                    <a href="#" className="text-sm text-[#D09A40] hover:underline">Forgot password?</a>
                                 </div>
 
                                 {/* Submit Button */}
                                 <button
                                     type="submit"
-                                    className="w-full lg:mt-12 mt-5 cursor-pointer  text-white py-3 px-2 rounded-[37px] btnColor text-lg font-bold "
+                                    className="w-full lg:mt-12 mt-5 cursor-pointer  text-white py-4 px-2 rounded-[8px] btnColor text-lg font-bold "
                                 >
                                     Login
                                 </button>
                             </form>
-                            <div>
-                                <p className=" text-[#697079] font-medium  block text-center text-[17px] lg:mt-10 mt-5 " >
-                                    Don’t have an account ? <Link className=" text-[#D09A40]  underline " href={"/auth/registration"} >Sign up</Link>
-                                </p>
-                            </div>
                         </div>
-                    </div>
 
-                    {/* Right Side: Image */}
-                    <div className="flex-1 w-full  lg:ax-w-[50%] mx-auto    ">
-                        <Image
-                            src="/images/login/login-image.jpg"
-                            alt="Login Image"
-                            width={700}
-                            height={700}
-                            className="w-full lg:h-[941px] object-cover rounded-[20px] "
-                        />
+
+                        {/* <div>
+                            <p className=" text-[#697079] font-medium  block text-center text-[17px] lg:mt-10 mt-5 " >
+                                Don’t have an account ? <Link className=" text-[#D09A40]  underline " href={"/auth/registration"} >Sign up</Link>
+                            </p>
+                        </div> */}
                     </div>
                 </div>
+
+
+
 
             </MaxWidth>
         </div>
